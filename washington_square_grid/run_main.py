5c2a06d7be2bf9af17c4f492b76eb8ab4795983f
Shivam Mishra <37779260+shivAM2886@users.noreply.github.com>
2021-04-06 02:27:32 -0400

    Delete run_main.py

--- washington_square_grid/run_main.py
+++
@@ -1,9 +0,0 @@
-from generate_grid_washington_dc import generate_grid
-
-# grid size in meters
-
-grid_size = 400
-
-
-df_result = generate_grid(grid_size).run_main()
-df_result.to_excel("output9.xlsx")
